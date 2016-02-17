95f218e6fae98afe35d48eaa427fd9df55491044
Ning, Tao <ningtaous@gmail.com>
2016-02-16 19:32:51 -0500

    Add insert / find method to mongodb

--- mongodb/testMongodb.js
+++ mongodb/testMongodb.js
@@ -4,6 +4,30 @@ var url = 'mongodb://localhost:27017/test';
 
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  db.close();
+  console.log('Connected correctly to server.');
+
+  db.collection('tao').insert({
+    name: 'tao',
+    age: 19
+  }, function(error, docs) {
+    if (error) {
+      console.log(error);
+      process.exit(1);
+    }
+
+    console.log('Insert successful.');
+
+    db.collection('tao').find().toArray(function(error, docs) {
+      if (error) {
+        console.log(error);
+        process.exit(1);
+      }
+
+      console.log('Found docs:');
+      docs.forEach(function(doc) {
+        console.log(JSON.stringify(doc));
+      });
+      db.close();
+    });
+  });
 });
