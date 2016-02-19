f7ec86ad56ae406b99c91fb2045a9f9f2ca103c0
Ning, Tao <ningtaous@gmail.com>
2016-02-19 15:55:39 -0500

    Set up express server for new homepage

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
-// var cool = require('cool-ascii-faces');
-// var express = require('express');
-// var app = express();
-//
-// app.set('port', (process.env.PORT || 5000));
-//
-// app.use(express.static(__dirname + '/public'));
-//
-// // views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-//
-// app.get('/', function(request, response) {
-//   response.render('pages/index');
-// });
-//
-// app.get('/cool', function(request, response) {
-//   response.send(cool());
-// });
-//
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
-
-
+var cool = require('cool-ascii-faces');
 var express = require('express');
-var wagner = require('wagner-core');
-
-require('./models')(wagner);
-
 var app = express();
 
-app.use('/api/v1', require('./api')(wagner));
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(3000);
-console.log('Listening on port 3000!');
+app.use(express.static(__dirname + '/public-html'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
