067465e2a3575890692ee3c2a56c0a48baf552e9
Ning, Tao <ningtaous@gmail.com>
2016-03-15 15:06:48 -0400

    feat: Inplement gulp for angular

--- angular/gulpfile.js
+++ angular/gulpfile.js
@@ -1,16 +1,40 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 
-gulp.task('browserify', function() {
+var del = require('del');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('clean', function() {
+  return del([
+    '../public/angular/dist/**/*',
+    '../public/angular/views/'
+  ], {force: true});
+});
+
+gulp.task('views', function() {
+  return gulp.
+    src(['views/**/*']).
+    pipe(gulp.dest('../public/angular/views'));
+})
+
+gulp.task('styles', function() {
+  return gulp.src('style/*')
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('../public/angular/dist/css/'));
+});
+
+gulp.task('scripts', function() {
   return gulp.
     src('js/app.js').
     pipe(browserify()).
-    pipe(gulp.dest('../public/angular/dist'));
+    pipe(uglify()).
+    pipe(gulp.dest('../public/angular/dist/js/'));
 });
 
 gulp.task('watch', function() {
   gulp.watch(['js/*.js', 'index.html'], ['browserify']);
 });
 
-gulp.task('default',['browserify']);
+gulp.task('default',['clean', 'views', 'styles', 'scripts']);
 
