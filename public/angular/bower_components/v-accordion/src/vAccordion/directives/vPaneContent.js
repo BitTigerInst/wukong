3987a7fb960796ce78ed1f19eef7eac81bcdd626
Ning, Tao <ningtaous@gmail.com>
2016-03-30 08:26:47 -0400

    chore: add bower_component to public by modify .gitignore

---
+++ public/angular/bower_components/v-accordion/src/vAccordion/directives/vPaneContent.js
@@ -0,0 +1,19 @@
+
+
+// vPaneContent directive
+angular.module('vAccordion.directives')
+  .directive('vPaneContent', vPaneContentDirective);
+
+
+function vPaneContentDirective () {
+  return {
+    restrict: 'E',
+    require: '^vPane',
+    transclude: true,
+    template: '<div ng-transclude></div>',
+    scope: {},
+    link: function (scope, iElement, iAttrs) {
+      iAttrs.$set('role', 'tabpanel');
+    }
+  };
+}
