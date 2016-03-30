8748ea99776a53adc5e2451c892a8b107cec5e96
Ning, Tao <ningtaous@gmail.com>
2016-03-30 05:58:21 -0400

    style: chang style in gulpfile.js according to eslint

--- angular/gulpfile.js
+++ angular/gulpfile.js
@@ -1,51 +1,49 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var runSequence = require('run-sequence');
-
 var del = require('del');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
   return del([
     '../public/angular/dist/**/*',
     '../public/angular/views/',
     '../public/angular/image/'
-  ], {force: true});
+  ], { force: true });
 });
 
-gulp.task('views', function() {
+gulp.task('views', function () {
   return gulp.src(['views/**/*']).
     pipe(gulp.dest('../public/angular/views/'));
-})
+});
 
-gulp.task('images', function() {
+gulp.task('images', function () {
   return gulp.src(['image/*']).
     pipe(gulp.dest('../public/angular/image/'));
-})
+});
 
-gulp.task('background', function() {
+gulp.task('background', function () {
   return gulp.src(['style/images/*']).
     pipe(gulp.dest('../public/angular/dist/css/images/'));
-})
+});
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
   return gulp.src('style/**/*.css')
     .pipe(concat('all.css'))
     .pipe(gulp.dest('../public/angular/dist/css/'));
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
   return gulp.src('js/app.js').
     pipe(browserify()).
     pipe(gulp.dest('../public/angular/dist/js/'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch(['js/*.js', 'index.html'], ['browserify']);
 });
 
-gulp.task('default', function(callback) {
-  runSequence('clean', 'views','images','background', 'styles', 'scripts', callback);
+gulp.task('default', function (callback) {
+  runSequence('clean', 'views', 'images', 'background', 'styles', 'scripts', callback);
 });
 
