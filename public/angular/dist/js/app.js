2b9c8177fc24cd24060ad44f61c9f8ff4c5418f4
Ning, Tao <ningtaous@gmail.com>
2016-04-04 09:10:35 -0400

    fix: rebuild public folder by run gulp in angular/

--- public/angular/dist/js/app.js
+++ public/angular/dist/js/app.js
@@ -3,6 +3,15 @@ exports.WelcomeTitleController = function($scope, $http, $timeout){
   $scope.title = "Welcome Back/ Welcome new hacker";
 };
 
+
+exports.TextDividerController= function($scope){
+  $scope.text = "Recent Updates";
+  $scope.description_l1 = "Below is the detail of the latest interesting projects and talented hackers.";
+  $scope.description_l2 = "To see more, Please find a study group to join.";
+};
+
+
+
 exports.UserAttributeBoard = function($scope) {
   $scope.range = [
     { label: 'Beginner'},
@@ -21,10 +30,7 @@ exports.DynamicNewsController = function($scope){
     { header: "Today's Lecture", content: "Git (/ɡɪt/[5]) is a widely used source code management system for software development. It is a distributed revision control system with an emphasis on speed,[6] data integrity,[7] and support for distributed, non-linear workflows.[8] Git was initially designed and developed in 2005 by Linux kernel developers (including Linus Torvalds) for Linux kernel development.[9]" },
     { header: "Ongoing Projects", content: "Hatch Mott MacDonald specializes in developing parking plans and parking management programs. Our team has extensive experience in developing sustainable parking solutions, drafting policies and regulatory language for parking programs, helping the clients understand the true cost of parking, offering site specific parking solutions, providing parking restoration and management services, and parking system monetization."},
     { header: "Tomorrow's event", content:  "btw, this post is a bit of a followup to my previous post,s, I found a little clarity & content ☺)Update (8/04/15): I’ve created an updated version for Angular-Material 0.10.0 on CodePen."},
-    { header: "Next 7 days Events", content: "", subpane: [
-    { eventsheader: "Mobile RTC Hackathon", eventscontent: "Events content"},
-    { eventsheader: "[太阁三人行]解读小米视频电话的黑科技：Agora.io", eventscontent: "Events content2"}
-    ]}
+    { header: "Next 7 days Events", content: "Mobile RTC Hackathon"}
   ];
 };
 
@@ -39,7 +45,7 @@ exports.ProjectListController = function($scope){
     {
       thumbnail: 'image/2.jpg',
       projectNumber: 'Option 2',
-      projectName: 'VIP Service Prediction (CloudBrain Challenge)',
+      projectName: 'VIP Service Prediction',
       description: 'There are millions of apps in the iOS AppStore and Google Play store. '
     }
   ];
@@ -50,6 +56,13 @@ exports.welcomeTitle = function() {
   return {
     controller: 'WelcomeTitleController',
     templateUrl: '/views/welcome-title.html'
+  };
+};
+
+exports.textDivider = function(){
+  return{
+    controller: 'TextDividerController',
+    templateUrl: '/views/text-divider.html'
   };
 };
 
@@ -80,7 +93,7 @@ var directives = require('./directives');
 var services = require('./services');
 var _ = require('underscore');
 
-var components = angular.module('wukong.components', ['ng', 'ngMaterial','ngAnimate','vAccordion','ui.bootstrap']);
+var components = angular.module('wukong.components', ['ng', 'ngMaterial']);
 
 _.each(controllers, function(controller, name) {
   components.controller(name, controller);
@@ -96,13 +109,7 @@ _.each(services, function(factory, name) {
 
 var app = angular.module('wukong', ['wukong.components', 'ngRoute']);
 
-app.config(function ($mdThemingProvider) {
-$mdThemingProvider
-    .theme('default')
-    .primaryPalette('indigo')
-    .accentPalette('pink')
-    .warnPalette('red')
-});
+
 },{"./controllers":1,"./directives":2,"./services":4,"underscore":5}],4:[function(require,module,exports){
 
 },{}],5:[function(require,module,exports){
