7c5ac74d912aa73a7afa9a62cf4200bf176e3652
yuqinlear <yuqinlear@gmail.com>
2016-04-17 12:29:04 -0400

    feat: add regex to hit index.html
    #27

--- routes/angularPage.js
+++ routes/angularPage.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-router.get('/index.html', function(req, res) {
+router.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
