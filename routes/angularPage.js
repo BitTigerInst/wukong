d873201e696a7a6fc48e542fc8c812d7a6c5453a
Ning, Tao <ningtaous@gmail.com>
2016-04-20 06:35:55 -0400

    refactor: from paul:  - add regular expression  - remove lists in header

--- routes/angularPage.js
+++ routes/angularPage.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-router.get('/index.html', function(req, res) {
-  res.sendFile(path.join(__dirname, '../views/index.html'));
+router.get('/welcome.html', function(req, res) {
+  res.sendFile(path.join(__dirname, '../views/welcome.html'));
 });
 
 module.exports = router;
--- routes/staticPages.js
+++ routes/staticPages.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/homepage.html', function(req, res) {
+router.get(/^\/(index(.html)?)?$/, function(req, res) {
   res.render('pages/homepage');
 });
 
